075b5ace8c0f9cf3ec3e6987b388e384183151ea
chenlu <chenlu@pjlab.org.cn>
2024-05-14 15:40:26 +0800

    feat: 更新guide

--- frontend/scripts/guide.ts
+++ frontend/scripts/guide.ts
@@ -8,6 +8,11 @@ const options = [
     message: '请选择要执行的命令',
     choices: [
       {
+        key: 0,
+        name: '启动登录页（login）',
+        value: 'start:login',
+      },
+      {
         key: 1,
         name: '启动供应商端（supplier）',
         value: 'start:supplier',
@@ -16,11 +21,6 @@ const options = [
         key: 2,
         name: '启动运营端（operator）',
         value: 'start:operator',
-      },
-      {
-        key: 'login',
-        name: '启动登录页（login）',
-        value: 'start:login',
       },
       {
         key: 3,
